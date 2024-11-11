50cf06ab4c123045d205f36b5fc1846bc1373a28
gabrielmrib7 <gabrielmrib@hotmail.com>
2024-11-11 04:46:09 -0300

    Finalizado!

--- resources/views/adicionar.blade.php
+++ resources/views/adicionar.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>ADICIONAR CEP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
@@ -44,7 +44,7 @@
     <label>Estado</label>
     <input type="text" class="form-control" name="estado" value="{{$estado}}">
 </div>
-  <button type="submit" class="btn btn-primary mt-3">Salvar</button>
+  <button type="submit" class="btn btn-dark  mt-3">Salvar</button>
 </form>
     </div>
 
